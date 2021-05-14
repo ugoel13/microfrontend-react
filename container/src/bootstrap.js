03a2219e5b0ec1127327d6f7aef509f576cf4aea
Ujjwal Goel <ujjwal.goel@publicissapient.com>
2021-05-15 02:20:50 +0530

    mount function changes in products and cart

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,3 +1,6 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import {mount} from 'products/ProductsIndex';
+import {mount as mountCart} from 'cart/CartShow';
 console.log('container');
+
+mount(document.querySelector('#my-products'));
+mountCart(document.querySelector('#my-cart'));
